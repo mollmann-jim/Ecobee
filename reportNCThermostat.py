03cb6bdf9386e2025107f99bc95ac8ff8762b253
Jim Mollmann <jim.mollmann@gmail.com>
2023-08-18 09:10:45 -0400

    ExtendedRuntime Object is a better source of data for the report. rewrite the report script

--- reportNCThermostat.py
+++ reportNCThermostat.py
@@ -20,14 +20,14 @@ def fmtTempsLine(tag, row):
     line = tag + ': (none)'
     if row['minT']:
         period =  '{:>10s}'.format(tag)
-        minT   = ' {:>5d}'.format(row['minT'])   if row['minT'] is not None else noData
-        maxT   = ' {:>5d}'.format(row['maxT'])   if row['maxT'] is not None else noData
-        avgT   = ' {:>5.1f}'.format(row['avgT']) if row['avgT'] is not None else noData
-        minC   = ' {:>-5d}'.format(row['minC'])  if row['minC'] is not None else noData
-        maxC   = ' {:>5d}'.format(row['maxC'])   if row['maxC'] is not None else noData
-        avgC   = ' {:>5.1f}'.format(row['avgC']) if row['avgC'] is not None else noData
-        minH   = ' {:>5d}'.format(row['minH'])   if row['minH'] is not None else noData
-        maxH   = ' {:>5d}'.format(row['maxH'])   if row['maxH'] is not None else noData
+        minT   = ' {:>5.1f}'.format(row['minT'])     if row['minT'] is not None else noData
+        maxT   = ' {:>5.1f}'.format(row['maxT'])     if row['maxT'] is not None else noData
+        avgT   = ' {:>5.1f}'.format(row['avgT'])     if row['avgT'] is not None else noData
+        minC   = ' {:>-5d}'.format(int(row['minC'])) if row['minC'] is not None else noData
+        maxC   = ' {:>5d}'.format(int(row['maxC']))  if row['maxC'] is not None else noData
+        avgC   = ' {:>5.1f}'.format(row['avgC'])     if row['avgC'] is not None else noData
+        minH   = ' {:>5d}'.format(int(row['minH']))  if row['minH'] is not None else noData
+        maxH   = ' {:>5d}'.format(int(row['maxH']))  if row['maxH'] is not None else noData
         avgH   = ' {:>5.1f}'.format(row['avgH']) if row['avgH'] is not None else noData
         line = period + minT + maxT + avgT + minC + maxC + avgC + minH + maxH + avgH
     return line
@@ -35,10 +35,14 @@ def fmtTempsLine(tag, row):
 def fmtRunTmLine(x):
     
     line = ''
+    noData = '     .'
     if x['elapsed'] > 0:
-        heatPct = '{:>6.1f}'.format(100.0 * x['heat']  / x['elapsed'])
-        coolPct = '{:>6.1f}'.format(100.0 * x['cool']  / x['elapsed'])
-        fanPct  = '{:>6.1f}'.format(100.0 * x['fanOn'] / x['elapsed'])
+        heatPct = '{:>6.1f}'.format(100.0 * x['heat']  / x['elapsed']) \
+            if x['heat']  is not None else noData
+        coolPct = '{:>6.1f}'.format(100.0 * x['cool']  / x['elapsed']) \
+            if x['cool']  is not None else noData
+        fanPct  = '{:>6.1f}'.format(100.0 * x['fanOn'] / x['elapsed']) \
+            if x['fanOn'] is not None else noData
         line = heatPct + coolPct + fanPct
     return line
 
@@ -52,93 +56,77 @@ def printHeader():
 def checkSanity(runStats, date, where):
     global insaneUsage
     for which in ['heat', 'cool', 'fanOn']:
-        runPct = 100.0 * runStats[which] / runStats['elapsed']
-        if runPct > saneUsageMax:
-            fmt = '\n{:>10s} - {:>10s} {:>4s} utilization of {:>5.1f}% exceeds the {:>5.1f}%' \
-                ' limit. Runtime = {:>8s}'
-            runTime = str(dt.timedelta(seconds = runStats[which]))
-            insaneUsage += fmt.format(date, where, which, runPct, saneUsageMax, runTime)
-            return True
+        if runStats[which] is not None:
+            runPct = 100.0 * runStats[which] / runStats['elapsed']
+            if runPct > saneUsageMax:
+                fmt = '\n{:>10s} - {:>10s} {:>4s} utilization of {:>5.1f}% exceeds the {:>5.1f}%' \
+                    ' limit. Runtime = {:>8s}'
+                runTime = str(dt.timedelta(seconds = runStats[which]))
+                insaneUsage += fmt.format(date, where, which, runPct, saneUsageMax, runTime)
+                return True
     return False
                                                
-def runTimes(c, table, start, end):
-    select = 'SELECT statusTime, fanOn, outputStatus FROM ' + table +\
-        ' WHERE statusTime >= ? AND statusTime <= ? ;'
-    c.execute(select, (start, end))
-    result = c.fetchall()
-    fanTime = heatTime = coolTime = 0
-    first = last = previous = None
-    for r in result:
-        statTime = dt.datetime.strptime((r['statusTime']), '%Y-%m-%d %H:%M:%S')
-        # may not have data for the entire time range
-        # start, end if they are within 10 minutes. Otherwise first and last times.
-        if first is None:
-            if (statTime - start).total_seconds() < 600:
-                first = start
-                previous = start
-            else:
-                first = statTime
-                previous = statTime + dt.timedelta(minutes =5)
-        delta = (statTime - previous).total_seconds()
-        # account for missing data by assuming 5 minutes
-        if delta > 900: delta = 300
-        if r['outputStatus'] == 'cool on':
-            coolTime += delta
-        elif r['outputStatus'] == 'heat on':
-            heatTime += delta
-        elif r['fanOn'] == 1:
-            fanTime += delta
-        elif r['outputStatus'] == 'off' and (r['fanOn'] == 0 or r['fanOn'] is None):
-            # no fan/heat/cool - just idle
-            pass
-        elif r['outputStatus'] is None:
-            # BayWeb data
-            pass
-        else:
-            print('Unexpected: outputStatus:', r['outputStatus'], '\tfanOn:',  r['fanOn'])
-        previous = statTime
-    # in case no data (Monday)
-    if previous:
-        if (end - previous).total_seconds() < 600:
-            last = end
-        else:
-            last = previous
-        #print(start, end, first, last, (last - first).total_seconds())
-        elapsed = (last - first).total_seconds()
+def runTimes(c, table, start, end, fanTime, auxTime):
+    selectTime = 'SELECT min(dataTime) as first, max(dataTime) as last '\
+        ' FROM ' + table +\
+        ' WHERE dataTime >= ? AND dataTime <= ?'
+    c.execute(selectTime, (start, end))
+    result = c.fetchone()
+    if result['first'] is None or result['last'] is None:
+        return {'elapsed' : 0, 'heat' : 0, 'cool' : 0, 'fanOn': fanTime}
+    first = dt.datetime.strptime(result['first'], '%Y-%m-%d %H:%M:%S%z')
+    last  = dt.datetime.strptime(result['last'], '%Y-%m-%d %H:%M:%S%z')
+    selectHeat = 'SELECT sum(auxHeat1) as heat FROM ' + table +\
+        ' WHERE dataTime >= ? AND dataTime <= ? AND hvacMode = "heatStage1On";'
+    c.execute(selectHeat, (start, end))
+    result = c.fetchone()
+    if result is None:
+        heatTime = 0
     else:
-        elapsed = 0
+        heatTime = result['heat']
+    selectCool = 'SELECT sum(cool1) as cool FROM ' + table +\
+        ' WHERE dataTime >= ? AND dataTime <= ? AND hvacMode = "compressorCoolStage1On";'
+    c.execute(selectCool, (start, end))
+    result = c.fetchone()
+    if result is None:
+        cool = 0
+    else:
+        coolTime = result['cool']
+    elapsed = (last - first).total_seconds()
     return {'elapsed' : elapsed, 'heat' : heatTime, 'cool' : coolTime, 'fanOn': fanTime}
 
-def getYears(c, thermostat):
-    select_min_yr = 'SELECT min(statusTime) AS min FROM ' + thermostat + ';'
-    c.execute(select_min_yr)
-    min = c.fetchone()
-    first = dt.datetime.strptime(min['min'], '%Y-%m-%d %H:%M:%S')
-    select_max_yr = 'SELECT max(statusTime) AS max FROM ' + thermostat + ';'
-    c.execute(select_max_yr)
-    max = c.fetchone()
-    last = dt.datetime.strptime(max['max'], '%Y-%m-%d %H:%M:%S')
+def getYears(c, table):
+    select_min_max_yr = 'SELECT '\
+        'min(dataTime) AS min,  '\
+        'max(dataTime) AS max   '\
+        'FROM ' + table + ';'
+    c.execute(select_min_max_yr)
+    minmax = c.fetchone()
+    first = dt.datetime.strptime(minmax['min'], '%Y-%m-%d %H:%M:%S%z')
+    last  = dt.datetime.strptime(minmax['max'], '%Y-%m-%d %H:%M:%S%z')
     return first, last
 
-def makeSection(c, thermostat, title, byDay = False, byMonth = False, year = None):
+def makeSection(c, thermostat, table, title, byDay = False, byMonth = False, year = None):
     start, end, name = getTimeInterval.getPeriod(title, year = year)
-    selectFields =  'SELECT ' \
-        'date(statusTime)  AS date, '\
-        'max(temp)         AS maxT, '\
-        'min(temp)         AS minT, '\
-        'avg(temp)         AS avgT, '\
-        'max(coolSetPoint) AS maxC, '\
-        'min(coolSetPoint) AS minC, '\
-        'avg(coolSetPoint) AS avgC, '\
-        'max(heatSetPoint) AS maxH, '\
-        'min(heatSetPoint) AS minH, '\
-        'avg(heatSetPoint) AS avgH  '\
-        ' FROM ' + thermostat +\
-        ' WHERE statusTime >= ? AND statusTime <= ? '
+    selectFields = 'SELECT '           \
+        'date(dataTime)    AS date,   '\
+        'max(temperature)  AS maxT,   '\
+        'min(temperature)  AS minT,   '\
+        'avg(temperature)  AS avgT,   '\
+        'max(desiredCool)  AS maxC,   '\
+        'min(desiredCool)  AS minC,   '\
+        'avg(desiredCool)  AS avgC,   '\
+        'max(desiredHeat)  AS maxH,   '\
+        'min(desiredHeat)  AS minH,   '\
+        'avg(desiredHeat)  AS avgH,   '\
+        'sum(fan)          AS fanRun, '\
+        'sum(auxHeat1)     AS auxRun  '\
+        ' FROM ' + table +\
+        ' WHERE dataTime >= ? AND dataTime <= ? '
     select = selectFields + ' ;'
     # sqlite date(timestamp) returns the UTC date
-    selectByDay   = selectFields + ' GROUP BY substr(statusTime,1,10) ORDER BY statusTime DESC;'
-    selectByMonth = selectFields + ' GROUP BY substr(statusTime,1, 7) ORDER BY statusTime DESC;'
+    selectByDay   = selectFields + ' GROUP BY substr(dataTime,1,10) ORDER BY dataTime DESC;'
+    selectByMonth = selectFields + ' GROUP BY substr(dataTime,1, 7) ORDER BY dataTime DESC;'
     if byDay:
         c.execute(selectByDay, (start, end))
     elif byMonth:
@@ -153,7 +141,7 @@ def makeSection(c, thermostat, title, byDay = False, byMonth = False, year = Non
                                       dt.time.min)
             EOD = dt.datetime.combine(dt.datetime.strptime(record['date'], '%Y-%m-%d').date(), \
                                       dt.time.max)
-            dailyRunStats = runTimes(c, thermostat, BOD, EOD)
+            dailyRunStats = runTimes(c, table, BOD, EOD, record['fanRun'], record['auxRun'])
             lineRunTm = fmtRunTmLine(dailyRunStats)
             if checkSanity(dailyRunStats, record['date'], thermostat):
                 lineRunTm += ' High Usage'
@@ -169,160 +157,48 @@ def makeSection(c, thermostat, title, byDay = False, byMonth = False, year = Non
                 EOM = BOM.replace(month = BOM.month + 1, day = 1) - \
                     dt.timedelta(microseconds = 1)
             #print('byMonth', record['date'], BOM, EOM)
-            dailyRunStats = runTimes(c, thermostat, BOM, EOM)
+            dailyRunStats = runTimes(c, table, BOM, EOM, record['fanRun'], record['auxRun'])
             lineRunTm = fmtRunTmLine(dailyRunStats)
         else:
             lineTemps = fmtTempsLine(name, record)
-            lineRunTm = fmtRunTmLine(runTimes(c, thermostat, start, end))
+            lineRunTm = fmtRunTmLine(runTimes(c, table, start, end,
+                                              record['fanRun'], record['auxRun']))
         print(lineTemps + lineRunTm)
         
-def makeReport(c, thermostat):
-    first, last = getYears(c, thermostat)
+def makeReport(c, thermostat, table):
+    first, last = getYears(c, table)
     print('---------------------------', thermostat, '----------------------------')
     printHeader()
-    makeSection(c, thermostat, 'Today')
-    makeSection(c, thermostat, 'Prev7days', byDay = True)
+    makeSection(c, thermostat, table, 'Today')
+    makeSection(c, thermostat, table, 'Prev7days', byDay = True)
     printHeader()
     for period in ['This Week', 'Last Week', 'This Month', 'Last Month']:
-        makeSection(c, thermostat,  period)
+        makeSection(c, thermostat, table, period)
     for period in ['This Month', 'Last Month']:
         printHeader()
         for year in range(last.year, first.year - 1, -1):
-            makeSection(c, thermostat, period, year = year)
+            makeSection(c, thermostat, table, period, year = year)
     printHeader()
-    makeSection(c, thermostat, 'YearByMonth', byMonth = True)
-    makeSection(c, thermostat, 'LastYear')
+    makeSection(c, thermostat, table, 'YearByMonth', byMonth = True)
+    makeSection(c, thermostat, table, 'LastYear')
     printHeader()
     for year in range(last.year, first.year - 1, -1):
-        makeSection(c, thermostat, 'Year', year = year)
+        makeSection(c, thermostat, table, 'Year', year = year)
     print('')
-    makeSection(c, thermostat,  'All')
+    makeSection(c, thermostat, table,  'All')
 
     #printHeader()
     #makeSection(c, thermostat,  'All', byDay = True)
     
 def main():
-    #HoneywellDB = '/home/jim/tools/Honeywell/MBthermostat3.sql'
-    #BayWebDB    = '/home/jim/tools/Honeywell/MBthermo.sql'
-    #EcobeeDB    = '/home/jim/tools/Ecobee/MBthermostat.sql'
     EcobeeDB    = '/home/jim/tools/Ecobee/Thermostats.sql'
-    WorkingDB   = '/home/jim/tools/Ecobee/Working.sql'
-    WorkingDB   = ':memory:'
-    Create = 'CREATE TABLE IF NOT EXISTS ZZZZZZZZ (\n' +\
-        ' id             INTEGER PRIMARY KEY,      \n' +\
-        ' statusTime     INTEGER,                  \n' +\
-        ' temp           INTEGER,                  \n' +\
-        ' coolSetPoint   INTEGER DEFAULT NULL,     \n' +\
-        ' heatSetPoint   INTEGER DEFAULT NULL,     \n' +\
-        ' fanOn          INTEGER,                  \n' +\
-        ' outputStatus   TEXT                      \n' +\
-        ' )'
-    eCreate = 'CREATE TABLE IF NOT EXISTS ZZZZZZZZ (\n' +\
-        ' id             INTEGER PRIMARY KEY,       \n' +\
-        ' statusTime     INTEGER,                   \n' +\
-        ' temp           INTEGER,                   \n' +\
-        ' coolSetPoint   INTEGER DEFAULT NULL,      \n' +\
-        ' heatSetPoint   INTEGER DEFAULT NULL,      \n' +\
-        ' fanOn          INTEGER,                   \n' +\
-        ' outputStatus   TEXT,                      \n' +\
-        ' coolStatus     TEXT,                      \n' +\
-        ' heatStatus     TEXT                       \n' +\
-        ' )'
-    eInsert = 'INSERT INTO ZZZZZZZZ         \n' +\
-        '   ( statusTime,                   \n' +\
-        '     temp,                         \n' +\
-        '     coolSetPoint,                 \n' +\
-        '     heatSetpoint,                 \n' +\
-        '     outputStatus,                 \n' +\
-        '     coolStatus,                   \n' +\
-        '     heatStatus,                   \n' +\
-        '     fanOn)                        \n' +\
-        '  SELECT SUBSTR(statusTime,1,19) AS statusTime,   \n' +\
-        '         ROUND(temp)             AS temp,         \n' +\
-        '         ROUND(coolSetPoint)     AS coolSetPoint, \n' +\
-        '         ROUND(heatSetpoint)     AS heatSetpoint, \n' +\
-        '         NULL                    AS outputStatus, \n' +\
-        '         coolStatus,                              \n' +\
-        '         heatStatus,                              \n' +\
-        '         fanOn                                    \n' +\
-        '    FROM Edb.YYYYYYYY                             \n' +\
-        '    ORDER BY statusTime                           \n'
-
-    Insert = 'INSERT INTO ZZZZZZZZ          \n' +\
-        '   ( statusTime,                   \n' +\
-        '     temp,                         \n' +\
-        '     coolSetPoint,                 \n' +\
-        '     heatSetpoint,                 \n' +\
-        '     outputStatus,                 \n' +\
-        '     fanOn)                        \n' +\
-        '  SELECT                           \n' +\
-        '     statusTime,                   \n' +\
-        '     temp,                         \n' +\
-        '     coolSetPoint,                 \n' +\
-        '     heatSetpoint,                 \n' +\
-        '     outputStatus,                 \n' +\
-        '     fanOn                         \n' +\
-        '    FROM eYYYYYYYY                 \n' +\
-        ';'
-    Index    = 'CREATE INDEX IF NOT EXISTS ZZZZZZZZindex ON YYYYYYYY (statusTime)'
-    UpdateC  = 'UPDATE ZZZZZZZZ SET coolSetPoint = NULL WHERE coolSetPoint == "null";'
-    UpdateH  = 'UPDATE ZZZZZZZZ SET heatSetPoint = NULL WHERE heatSetPoint == "null";'
-    UpdateT  = 'UPDATE ZZZZZZZZ SET temp         = NULL WHERE temp         == "null";'
-    UpdateEc = 'UPDATE ZZZZZZZZ SET outputStatus = "cool on" WHERE coolStatus IS NOT NULL;'
-    UpdateEh = 'UPDATE ZZZZZZZZ SET outputStatus = "heat on" WHERE heatStatus IS NOT NULL;'
-    db = sqlite3.connect(WorkingDB)
+    db = sqlite3.connect(EcobeeDB)
     db.row_factory = sqlite3.Row
     c = db.cursor()
-    #c.execute('ATTACH DATABASE "' + HoneywellDB + '" AS Hdb')
-    #c.execute('ATTACH DATABASE "' + BayWebDB    + '" AS Bdb')
-    c.execute('ATTACH DATABASE "' + EcobeeDB    + '" AS Edb')
-    
+        
     for thermostat in ['LivingRoom', 'Loft']:
-        table = 'main.' + thermostat
-        c.execute('DROP TABLE IF EXISTS ' + table)
-        create = Create.replace('ZZZZZZZZ', table)
-        #print(create)
-        c.execute(create)
-        
-        index = Index.replace('ZZZZZZZZ', table)
-        index = index.replace('YYYYYYYY', thermostat)
-        #print(index)
-        c.execute(index)
-        
-        # recode Ecobee coolStatus & heatStatus to outputStatus
-        etable = 'main.e' + thermostat
-        c.execute('DROP TABLE IF EXISTS ' + etable)
-        ecreate = eCreate.replace('ZZZZZZZZ', etable)
-        #print(ecreate)
-        c.execute(ecreate)
-        einsert = eInsert.replace('ZZZZZZZZ', etable)
-        einsert = einsert.replace('YYYYYYYY', thermostat)
-        #print(einsert)
-        c.execute(einsert)
-        db.commit()
-        updateEc = UpdateEc.replace('ZZZZZZZZ', etable)
-        #print(updateEc)
-        c.execute(updateEc)
-        db.commit()
-        updateEh = UpdateEh.replace('ZZZZZZZZ', etable)
-        #print(updateEh)
-        c.execute(updateEh)
-        db.commit()
-        
-        insert = Insert.replace('ZZZZZZZZ', table)
-        insert = insert.replace('YYYYYYYY', thermostat)
-        #print(insert)
-        c.execute(insert)
-        db.commit()
-        update = UpdateC.replace('ZZZZZZZZ', table)
-        c.execute(update)
-        update = UpdateH.replace('ZZZZZZZZ', table)
-        c.execute(update)
-        update = UpdateT.replace('ZZZZZZZZ', table)
-        c.execute(update)
-        db.commit()
-        #db.set_trace_callback(print)
-        makeReport(c, thermostat)
+        table = thermostat + 'X'
+        makeReport(c, thermostat, table)
 
     print(insaneUsage)
 
