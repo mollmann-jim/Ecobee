c6b272d5644e8aa4156c553742dbd5d3c2e6be5d
Jim Mollmann <jim.mollmann@gmail.com>
2024-04-15 09:21:49 -0400

    Add backup mode so EcoBee.py can run in NMB. It does nothing unless access to home(NC) is down.
    Comment out code that deleted and readded vacations around
    adding a Climate_hold while in vacation mode. Done to insure
    some air movement/dehumidification daily. Turns out that we
    can just layer the Cilmate_hold on top of the vacation.
    
    Dump the "event" setion after adding the climate hold.

--- EcoBee.py
+++ EcoBee.py
@@ -14,7 +14,8 @@ import time
 import os
 import sys
 import sched
-from traceback import print_exc
+import subprocess
+from traceback import print_exc, print_stack
 
 def setLogging(logger):
     global LOGFILE
@@ -43,6 +44,45 @@ def findSubstr(s1, s2):
             return l[i]
     return None
 
+def doCmd(command, printFailure = True, debug = False):
+    if debug: print('doCmd:command:', command)
+    result = subprocess.run(command, shell = True, stdout = subprocess.PIPE, \
+                            stderr=subprocess.STDOUT)
+    if result.returncode != 0 & printFailure:
+        #print(result.stdout)
+        print('Failed: RC:', result.returncode, command)
+        print(result.stderr)
+    if debug: print('doCmd:result:', result)
+    return result
+
+backupModelastCheck = None
+backupModeActive    = False
+class backupMode:
+    def __init__(self, frequency = 900):
+        self.frequency = frequency
+        global backupModelastCheck, backupModeActive
+        backupModelastCheck = dt.datetime.now() - \
+            dt.timedelta(seconds = 1 + self.frequency)
+        backupModeActive = False
+
+    def active(self):
+        global backupModelastCheck, backupModeActive
+        host = doCmd('/usr/bin/hostname').stdout.decode('utf-8').rstrip('\n')
+        if host == 'jim4':
+            backupModeActive = False
+        else:   
+            if dt.datetime.now() - backupModelastCheck > dt.timedelta(seconds = self.frequency):
+                backupModelastCheck = dt.datetime.now()
+                cmd = '/usr/bin/ssh jim ps -efw | grep EcoBee.py'
+                result = doCmd(cmd)
+                #print(result.returncode)
+                if result.returncode == 0:
+                    backupModeActive = True
+                else:
+                    backupModeActive = False
+        #print('backupMode.active:', backupModeActive)
+        return backupModeActive
+    
 class fdPrint:
     def __init__(self, fd):
         self.fd = fd
@@ -545,6 +585,7 @@ class collectThermostatData:
     def __init__(self, scheduler):
         self.scheduler = scheduler
         self.starttime = 0
+        self.backupMode = backupMode()
         
     def Schedule(self, Getter, Saver, API, hours = 0, minutes = 0, seconds = 0):
         self.frequency = dt.timedelta(hours = hours, minutes = minutes, seconds = seconds)
@@ -564,6 +605,9 @@ class collectThermostatData:
         # reschedule
         self.starttime = self.starttime + self.frequency
         self.scheduler.enterabs(time.mktime(self.starttime.timetuple()), 1, self.Collector, ())
+        if self.backupMode.active():
+            print('backupMode.active: skipping Collector')
+            return
         self.Getter()
         self.Saver(self.API)
 
@@ -573,6 +617,7 @@ class deHumidify:
         self.where     = where
         self.starttime = 0
         self.thermostats = thermostats
+        self.backupMode = backupMode()
         self.pp = pprint.PrettyPrinter(indent=4, sort_dicts=False)
         
     def Schedule(self, API, Status, startHour = 6, startMinute = 30, duration = 30):
@@ -607,6 +652,10 @@ class deHumidify:
         self.scheduler.enterabs(time.mktime(self.starttime.timetuple()), 1, self.forceRun, ())
         #print('deHumidify.forceRun next:', self.starttime)
 
+        if self.backupMode.active():
+            print('backupMode.active: skipping forceRun')
+            return
+
         for i in range(len(self.API.thermostats)):
             if self.API.thermostats[i]['name'] not in self.thermostats:
                 continue
@@ -639,10 +688,12 @@ class deHumidify:
                                                                        climate)
         self.Status(note)
 
+        '''
         cool_temp = [82] * len(self.API.thermostats)
         heat_temp = [50] * len(self.API.thermostats)
         end_date  = [None] * len(self.API.thermostats)
         end_time  = [None] * len(self.API.thermostats)
+        self.debugVacation(myThermos, hdr = 'before delete')
         for i in myThermos:
             vacName      = self.API.thermostats[i]['events'][0]['name']
             cool_temp[i] = self.API.thermostats[i]['events'][0]['coolHoldTemp']
@@ -653,8 +704,10 @@ class deHumidify:
         time.sleep(10)
         self.API.getThermostatData()
         self.Status('deHumidify: vacation deleted: ' + vacName)
-
+        self.debugVacation(myThermos, hdr = 'after delete')
+        '''
         finish = dt.datetime.now() + dt.timedelta(minutes = self.duration)
+        '''
         # add new vacation to start after hold
         print('hhh: create vacation', myThermos, self.where, cool_temp, heat_temp, end_date, end_time)
         for i in myThermos:
@@ -669,12 +722,14 @@ class deHumidify:
                                      )
         time.sleep(10)
         self.API.getThermostatData()
+        self.debugVacation(myThermos, hdr = 'after adding back')
         for i in myThermos:
             self.Status('deHumidify: vacation created ' +
                         str(int(cool_temp[i]) / 10) + ' / ' +
                         str(int(heat_temp[i]) / 10))
             time.sleep(2)
         #self.pp.pprint(self.API.thermostats)
+        '''
 
         for i in myThermos:
             self.API.Set_Climate_hold(index = i,
@@ -686,8 +741,15 @@ class deHumidify:
             time.sleep(5)
         time.sleep(10)
         self.API.getThermostatData()
+        self.debugVacation(myThermos, hdr = 'after adding climate hold')
         self.Status('deHumidify: climate hold added')
-        self.pp.pprint(self.API.thermostats)
+        #self.pp.pprint(self.API.thermostats)
+
+    def debugVacation(self, Thermos, hdr = 'no header'):
+        print('debugVacation: ', hdr)
+        for i in Thermos:
+            self.pp.pprint(self.API.thermostats[i]['name'])
+            self.pp.pprint(self.API.thermostats[i]['events'])
         
         
 def main():
@@ -738,9 +800,15 @@ def main():
 
     NCdehumidify = deHumidify(scheduler, thermostats = NCthermostats, where = 'NC')
     SCdehumidify = deHumidify(scheduler, thermostats = SCthermostats, where = 'SC')
-    NCdehumidify.Schedule(API, NCstatus.addLine, startHour = 6, startMinute = 30, duration = 60)
-    SCdehumidify.Schedule(API, SCstatus.addLine, startHour = 6, startMinute = 45, duration = 60)
-
+    host = os.getenv('HOSTNAME')
+    if host == 'jim4':
+        NCdehumidify.Schedule(API, NCstatus.addLine, startHour = 6, startMinute = 30, duration = 60)
+        SCdehumidify.Schedule(API, SCstatus.addLine, startHour = 6, startMinute = 45, duration = 60)
+    else:
+        # if not tunning at "home", set start 5 minutes later
+        # should allow home to disable vacation mode, if it is running
+        NCdehumidify.Schedule(API, NCstatus.addLine, startHour = 6, startMinute = 35, duration = 60)
+        SCdehumidify.Schedule(API, SCstatus.addLine, startHour = 6, startMinute = 50, duration = 60)
     ###############3 debug
     '''
     DH = dt.datetime.now().replace(microsecond = 0) + dt.timedelta(minutes = 10)
