a064e310c4d2bc09afaaddf4d8cec4a1134e18b0
Jim Mollmann <jim.mollmann@gmail.com>
2025-07-08 18:33:40 -0400

    use $HOME; send report to SynologyDrive

--- reportNCThermostat.sh
+++ reportNCThermostat.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #set -x
-logDir="/home/jim/tools/Ecobee/logs"
-log=$logDir/report.$(/bin/date +%F-%T | /bin/tr : .);
-#/home/jim/tools/Ecobee/reportNMBThermostat.py > $log 2>&1
-/home/jim/tools/Ecobee/reportNCThermostat.py  > $log 2>&1
+logDir="$HOME/tools/Ecobee/logs"
+reportDir="$HOME/SynologyDrive/Reports.Daily/"
+log=$logDir/report.$(/bin/date +%F-%T | /usr/bin/tr : .);
+#$HOME/tools/Ecobee/reportNMBThermostat.py > $log 2>&1
+$HOME/tools/Ecobee/reportNCThermostat.py  > $log 2>&1
+cp -p $log $reportDir/NC.Thermostat.txt
+cp -p $log $reportDir/All/NC.Thermostat.$(basename -- "$log").txt
 cat $log
 # keep only the newest
 REMOVE=$(ls -t $logDir/report* | sed 1,20d)
@@ -18,5 +21,5 @@ else
 fi
 
 me=jim.mollmann@gmail.com
-(echo -e "Subject: $Alert NC Thermostat Usage Report: $(date)\n"; cat $log) | sendmail $me
+#### (echo -e "Subject: $Alert NC Thermostat Usage Report: $(date)\n"; cat $log) | sendmail $me
 
